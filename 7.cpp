4e63441b215ce8aaaecc104e1db2afdaf4a1b62d
yyqh <772296896@qq.com>
2019-12-27 22:46:27 +0800

    第二版

--- 7.cpp
+++ 7.cpp
@@ -1,14 +1,21 @@
 #include<stdio.h>
 int main()
 {
- int r;
- printf("请输入r：");
-while(~scanf_s("%d",&r))
-{
-	if(r>0)
-	printf("面积是：%f\n",2*3.1415*r*r);
-	else
-	printf("输入的r不合法！\n");
-}
- return 0;
+	int r;
+	printf("请输入半径r：");
+	while (~scanf_s("%d", &r))
+	{
+		if (r > 0)
+		{
+			printf("此圆的面积是：%f\n", 2 * 3.1415 * r * r);
+		}
+		else if (r == 0) {
+			return 0;
+		}
+		else
+		{
+			printf("输入的半径不合法！\n");
+		}
+	}
+	return 0;
 }
